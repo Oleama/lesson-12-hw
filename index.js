80fb6038643b28ca0d8a41a1388bf8a8f1599ca3
Olea <maliutina.olea@gmail.com>
2019-10-19 12:03:40 +0300

    1

---
+++ index.js
@@ -0,0 +1,14 @@
+var array = ["HTML", 11, "JavaScript", 2, "CSS", 23, 12];
+//var filtred = array.filter (function(number) {
+  //var result = (element = Number);
+  //return result ;
+//})
+
+array.push(-3);
+array.unshift(1000);
+array.push("Last array element");
+delete array[2];
+
+
+console.log(array);
+console.log(filtred);
